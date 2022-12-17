387b673da9ca942e7f63d54787e7fd7a32c4687b
carlosofscience <carlosofscience@gmail.com>
2022-12-16 19:43:09 -0500

    connecting to the database

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const {errorHandler} = require('./middleware/errorMiddleware')
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 8000;
 
-const app = express()
+//connecting to DB
+connectDB()
+
+const app = express();
 
 //adding middleware to parse request data to json
-app.use(express.json())
+app.use(express.json());
 //adding middlewar for url encoded form
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to support desk API!" });
@@ -18,8 +23,8 @@ app.get("/", (req, res) => {
 app.use("/api/users", require("./routes/userRoutes"));
 
 //API error handling
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
