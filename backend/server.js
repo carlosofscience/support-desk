f942b481ef91ab613be314fc5f76369bc2b75677
carlosofscience <carlosofscience@gmail.com>
2022-12-16 19:31:38 -0500

    adding async and custom error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 8000;
 
 const app = express()
+
+//adding middleware to parse request data to json
+app.use(express.json())
+//adding middlewar for url encoded form
+app.use(express.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to support desk API!" });
@@ -10,6 +16,9 @@ app.get("/", (req, res) => {
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
+
+//API error handling
+app.use(errorHandler)
 
 app.listen(PORT, ()=>{
   console.log(`Server started on port ${PORT}`);
