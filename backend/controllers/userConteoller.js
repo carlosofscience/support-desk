0838fefd2e7cba4ea4d09dfd4d12d9b5bf035665
carlosofscience <carlosofscience@gmail.com>
2022-12-16 20:01:11 -0500

    adding user registration

--- backend/controllers/userConteoller.js
+++ backend/controllers/userConteoller.js
@@ -1,17 +1,48 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
 
+const User = require('../models/userModel')
 
 // @desc    Register a new user
 // @route   /api/users
 // @access  Public
 const registerUser = asyncHandler( async (req, res) => {
-  console.log(req.body);
+  
   const {name, email, password} = req.body
+  
   if(!name || !email ||!password){
     res.status(400)
     throw new Error('Please include all fields!')
   }
-  res.send('Register Route');
+
+  //Find if user already exists
+  const userExists = await User.findOne({email})
+  if(userExists){
+    res.status(400)
+    throw new Error('User already Exists')
+  }
+
+  //Hash passoword
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  //Create User
+  const user =  await User.create({
+    name, 
+    email, 
+    password: hashedPassword
+  })
+
+  if(user){
+    res.status(201).json({
+      _id:    user._id,
+      name:   user.name,
+      email:  user.email
+    });
+  }else{
+    res.status(400)
+    throw new Error('Invalid User Data');
+  }
 })
 
 
